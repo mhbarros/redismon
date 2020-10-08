7fe5dfe7f8d1b0cdea4bebcfbddb0c092b830581
Marcelo Barros <mhbarros99@gmail.com>
2020-10-07 22:22:41 -0300

    refatoração 1

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
-const redis = require('redis');
+
+require('./js/ipcMainListener');
+
 require('electron-reload')(__dirname);
 
 const start = () => {
-  const w = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
     webPreferences: {
@@ -14,23 +16,7 @@ const start = () => {
     icon: path.join(__dirname, 'img', 'redis.png')
   });
 
-  w.loadFile('pages/index.html');
-  // w.webContents.openDevTools();
+  mainWindow.loadFile('pages/index.html');
 
 }
 app.whenReady().then(start);
-
-ipcMain.on('connectToRedis', (listener, args) => {
-  const client = redis.createClient(args.port, args.host);
-  client.on('connect', () => {
-    console.log('Conectado');
-    listener.reply('redisConnected', 'Conectado com sucesso');
-  })
-
-  global.RedisClient = client;
-})
-
-ipcMain.on('logout', (listener, args) => {
-  global.RedisClient = undefined;
-  listener.reply('logout');
-});
